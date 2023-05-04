dba9456bfe1600aece2b57ca9da48e84af6a5174
Imola Saraine Lajos <Imola_Saraine_Lajos@epam.com>
2023-05-05 00:31:07 +0200

    add command line runner for task 1

---
+++ src/main/java/com/imola/saraine/lajos/module5/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.imola.saraine.lajos.module5;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class HelloWorldController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        log.info("Hello world controller is called");
+        return "Hello world!";
+    }
+}
--- src/main/java/com/imola/saraine/lajos/module5/Module5Application.java
+++ src/main/java/com/imola/saraine/lajos/module5/Module5Application.java
@@ -1,13 +1,37 @@
 package com.imola.saraine.lajos.module5;
 
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@Slf4j
 public class Module5Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Module5Application.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner run() {
+		return args -> {
+			System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+			log.info("The command line runner started");
+			System.out.println("Hello world from the command line runner");
+			System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+		};
+	}
+
+	@PostConstruct
+	public void init() {
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		log.info("Post construct method running");
+		System.out.println("This is the post construct method");
+		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 	}
 
 }
