6d0ac4d40531a7631abbd3b0da2015294d542513
Imola Saraine Lajos <Imola_Saraine_Lajos@epam.com>
2023-05-21 23:33:43 +0200

    fix integration test csrf issue

--- src/test/java/com/imola/saraine/lajos/module5/controller/HeadacheControllerTest.java
+++ src/test/java/com/imola/saraine/lajos/module5/controller/HeadacheControllerTest.java
@@ -1,38 +1,41 @@
 package com.imola.saraine.lajos.module5.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.imola.saraine.lajos.module5.model.Headache;
 import com.imola.saraine.lajos.module5.model.HeadacheType;
 import com.imola.saraine.lajos.module5.service.HeadacheService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.time.LocalDate;
-import java.util.Collections;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ExtendWith(SpringExtension.class)
 @WebMvcTest(HeadacheController.class)
-@ActiveProfiles("oauth2")
 class HeadacheControllerTest {
 
     private Headache headache;
 
+    private static final long ID_1 = 1L;
     @Autowired
     private MockMvc mockMvc;
-
+    @Autowired
+    private ObjectMapper objectMapper;
     @MockBean
     private HeadacheService headacheService;
 
@@ -40,32 +43,50 @@ class HeadacheControllerTest {
     @BeforeEach
     void setUp() {
         headache = Headache.builder()
-                .id(1L)
+                .id(ID_1)
                 .type(HeadacheType.MIGRAINE)
                 .strength(5)
                 .occurance(LocalDate.now())
                 .build();
-        OAuth2User principal = new DefaultOAuth2User(
-                Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")),
-                Collections.singletonMap("name", "testUser"),
-                "name"
-        );
-
-        SecurityContextHolder.getContext().setAuthentication(
-                new OAuth2AuthenticationToken(principal, Collections.emptyList(), "github")
-        );
 
     }
     @Test
+    @WithMockUser
     void lookUpHeadache() throws Exception {
-        Long id = 1L;
-        headache.setId(id);
+        when(headacheService.lookUpHeadache(ID_1)).thenReturn(headache);
 
-        when(headacheService.lookUpHeadache(id)).thenReturn(headache);
-
-        mockMvc.perform(MockMvcRequestBuilders.get("/headaches/" + id)
+        mockMvc.perform(MockMvcRequestBuilders.get("/headaches/" + ID_1)
                         .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(id));
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(ID_1));
+    }
+
+
+    @Test
+    @WithMockUser
+    public void markDownHeadache() throws Exception {
+        when(headacheService.markDownHeadache(any(Headache.class))).thenReturn(headache);
+
+        mockMvc.perform(post("/headaches")
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(headache))
+                        .with(csrf()))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.strength").value(headache.getStrength()));
+    }
+
+    @Test
+    @WithMockUser
+    void testChangeHeadacheStrength() throws Exception {
+
+        Headache updatedHeadache = new Headache(ID_1, HeadacheType.MIGRAINE, 7, LocalDate.now());
+        when(headacheService.changeHeadacheStrength(ID_1, 7)).thenReturn(updatedHeadache);
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/headaches/{id}/strength/{strength}", ID_1, 7)
+                    .with(csrf()))
+
+
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.strength").value(updatedHeadache.getStrength()));
     }
 }
